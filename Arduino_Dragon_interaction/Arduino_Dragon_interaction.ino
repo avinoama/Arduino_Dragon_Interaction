041f4b145ab83651b5bfa3a3e29b1d0b9f62f092
Avinoam <avinoam@trackonomics.net>
2016-03-22 07:35:22 +0200

    added modes

--- Arduino_Dragon_interaction/Arduino_Dragon_interaction.ino
+++ Arduino_Dragon_interaction/Arduino_Dragon_interaction.ino
@@ -20,7 +20,7 @@ uint32_t color_map[] = {
   strips[0].Color(128, 0, 128),   // yellow
   strips[0].Color(0, 0, 255),     // green
   strips[0].Color(255, 255, 0),   // Purple
-  strips[0].Color(255, 0, 0)      // Red 
+  strips[0].Color(255, 0, 0)      // Red
 };
 // Each LED strip will be light up like so:
 //       Yellow           Green          Purple          Red
@@ -63,10 +63,15 @@ int impactStatus = 0; // '0' no impact || '1' impact
 
 // if you want to see the serial output change this param
 bool debug = false;
+// mode is the game you play with the dragon
+// for now there are 2 game modes
+// 0 basic for kids
+// 1 mode advance
+int mode = 1 ;
 
 // the setup routine runs once when you press reset:
 void setup() {
-  if(debug) {  
+  if (debug) {
     Serial.begin(9600);
   }
   // for randome initilization
@@ -85,28 +90,28 @@ void setup() {
     digitalWrite(indicator_pins[i], HIGH);
     digitalWrite(indicator_pins[i], LOW);
   }
-  // Show impact avilable after impactTimeoutMin 
+  // Show impact avilable after impactTimeoutMin
   impactCountDown = impactTimeoutMin;
-  
-  // Setup Serial MP3 
+
+  // Setup Serial MP3
   serialmp3_init(); // Initialize sound
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  if(debug) {  
+  if (debug) {
     Serial.print("currentImpactCount ");  Serial.print(currentImpactCount); Serial.println();
   }
 
   // Check the sensor values of current impact zone
   for (int i = 0 ; i < currentImpactCount ; i++) {
     int j = impact_sensors[currentImpact[i]];
-    if(debug) {  
-        Serial.print("j ");  Serial.print(j); Serial.println();
+    if (debug) {
+      Serial.print("j ");  Serial.print(j); Serial.println();
     }
     sensorValues[j] = analogRead(j);
-    if(debug) {  
-      Serial.print("j ");  Serial.print(j); Serial.print("value ");Serial.print(sensorValues[j]); Serial.print("last ");Serial.print(sensorLastValues[j]);  Serial.println();
+    if (debug) {
+      Serial.print("j ");  Serial.print(j); Serial.print("value "); Serial.print(sensorValues[j]); Serial.print("last "); Serial.print(sensorLastValues[j]);  Serial.println();
     }
     if (sensorValues[j] > sensorLastValues[j] + impactThreshold) {
       impact(currentImpact[i], sensorValues[j]);
@@ -117,13 +122,50 @@ void loop() {
 
   // Delay impact surfaces changes
   impactCountDown--;
-  if(debug) {  
+  if (debug) {
     Serial.print("impactCountDown ");  Serial.print(impactCountDown); Serial.println();
   }
-  // change status of surfaces
+  // current game mode
+  switch (mo) {
+    case 0:
+      mode0();
+      break;
+    case 1:
+      mode1();
+      break;
+  }
+
+
+  delay(1);        // delay in between reads for stability use min 1
+}
+
+// mode0 is the basic interaction
+// you need to attack all body parts as fast as you can
+void mode0() {
   if (impactCountDown == 0) {
-    if(debug) {  
-        Serial.print("impactStatus ");  Serial.print(impactStatus); Serial.println();
+    if (impactStatus == 1) { // got a hit wait for one interval
+      currentImpactCount = 1;
+      impactStatus = 0;
+    }
+
+    indicators_off();
+
+    // write a for loop
+    for (int i = 0 ; i < currentImpactCount ; i++) {
+      currentImpact[i] = random(impact_sensors_count + 1);
+      digitalWrite(indicator_pins[currentImpact[i]], HIGH);
+    }
+
+
+    impactCountDown = random(impactTimeoutMin, impactTimeoutMax);
+  }
+}
+// mode1 is the basic interaction that every time X Sensor are lit up
+// i call it the Q mode because you wait for the Dragon to attack and then fight back
+void mode1() {
+  if (impactCountDown == 0) {
+    if (debug) {
+      Serial.print("impactStatus ");  Serial.print(impactStatus); Serial.println();
     }
     if (impactStatus == 1) { // got a hit wait for one interval
       currentImpactCount = 1;
@@ -131,48 +173,51 @@ void loop() {
     }
 
     currentImpact[0] = random(impact_sensors_count + 1);
-    if(debug) {  
-        Serial.print("currentImpact ");  Serial.print(currentImpact[0]); Serial.println();
+    if (debug) {
+      Serial.print("currentImpact ");  Serial.print(currentImpact[0]); Serial.println();
     }
     indicators_off();
-    
-    if(debug) {  
-       Serial.print("indicator_pins[currentImpact[0]] ");  Serial.print(indicator_pins[currentImpact[0]]); Serial.println();
+
+    if (debug) {
+      Serial.print("indicator_pins[currentImpact[0]] ");  Serial.print(indicator_pins[currentImpact[0]]); Serial.println();
     }
-    
+
     digitalWrite(indicator_pins[currentImpact[0]], HIGH);
 
 
     impactCountDown = random(impactTimeoutMin, impactTimeoutMax);
   }
-  
-  delay(1);        // delay in between reads for stability use min 1
 }
-
-
+// mode2 will be an interaction game that will work with patterns
+void mode2 () {
+}
 void impact(int position, int amount) {
-  if(debug) {  
+  if (debug) {
     Serial.print("Impact ");  Serial.print(position); Serial.print("\t"); Serial.println(amount); Serial.println();
   }
- 
-   // so i decided position 0 is the Head 
-   // Head shot both LED strip lights UP according to strangth
-   // Hand Gets hit only single LED strip is litten 
-   int a = map(amount, 0, 1024, 0, NUMPIXELS);
+
+  // so i decided position 0 is the Head
+  // Head shot both LED strip lights UP according to strangth
+  // Hand Gets hit only single LED strip is litten
+  int a = map(amount, 0, 1024, 0, NUMPIXELS);
   if (position == 0 || position == 2) { // left hand or head
-    if(debug) { Serial.print("left hand or head ");}
+    if (debug) {
+      Serial.print("left hand or head ");
+    }
     colorRainbow(color_map, ilumination_delay, a, 0);
   }
   if (position == 1 || position == 2) { // right hand or head
-    if(debug) {Serial.print("right hand or head ");}
+    if (debug) {
+      Serial.print("right hand or head ");
+    }
     colorRainbow(color_map, ilumination_delay, a, 1);
   }
-    
+
   //song = position + 2;
   song = random(1, 7);
   //  Serial.print("song ");  Serial.print(song); Serial.println();
   //  Serial.print("folder ");  Serial.print(folder); Serial.println();
-  
+
   serialmp3_play(folder, song);
   // delay(3000);
   // serialmp3_stop();
@@ -203,7 +248,7 @@ void colorRainbow(uint32_t color_map[], uint8_t wait, int until, int strip) {
     } else {
       color_pos = 3;
     }
-    
+
     strips[strip].setPixelColor(i, color_map[color_pos]);
     strips[strip].show();
     delay(wait);
